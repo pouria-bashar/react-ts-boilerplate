8c8cc403a54518a06a4a9837555f177633368e59
Pouria Bashardoust <pouria.bashardoust@gmail.com>
2020-10-24 10:35:04 +1100

    initial

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,11 @@ import * as React from 'react';// @ts-ignore
 const style = require('./style.css');
 
 const Button = () => {
-
   return (
     <button className={style.button}>
       Button
     </button>
   )
-
 }
 
 export default Button;
